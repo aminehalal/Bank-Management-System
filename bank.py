32e171b4793a56e3049f7e2855dd1a7bf21ddbe8
Amine Halal <aminenohalal@gmail.com>
2023-04-02 13:42:36 +0200

    5th commit

--- bank.py
+++ bank.py
@@ -51,6 +51,10 @@ class BankManagementSys :
 
                 amountnow = Label(mainframe , text="Your current balance is : " + str(amount[0]) + " DH" ,font=("times new roman",25,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
                 amountnow.place(x=0,y=100,width=600,height=60)
+
+            def logout():
+                new = BankManagementSys(root)
+
             cin = username.get()
             passwrd = password.get()
             db = sqlite3.connect(r"C:\Users\lenevo\Desktop\Langage\Python Projects\Project\Gestion de banque\bank.db")
@@ -85,6 +89,9 @@ class BankManagementSys :
                     btndep.place (x= 350 , y= 315 , width=250 , height=50)
 
                     balancen = balance.get()
+
+                    btndep = Button(mainframe ,text="Logout" , command=logout,font=("times new roman",30,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE )
+                    btndep.place (x= 200 , y= 380 , width=250 , height=50)
                     #methodes
             else :
                 self.errorlogindetail()
@@ -94,19 +101,19 @@ class BankManagementSys :
 
         # head picture
         imghead = Image.open(r"C:\Users\lenevo\Desktop\Langage\Python Projects\Project\Gestion de banque\images\head.jpg")
-        imghead = imghead.resize((1000,120),Image.ANTIALIAS)
+        imghead = imghead.resize((880,120),Image.ANTIALIAS)
         self.photoimghead = ImageTk.PhotoImage(imghead)
 
         lblimghead = Label(self.root,image=self.photoimghead,bd=4,relief=RIDGE)
-        lblimghead.place(x=0,y=0,width=1000,height=120)
+        lblimghead.place(x=120,y=0,width=880,height=120)
 
         #logo
         imglogo = Image.open(r"C:\Users\lenevo\Desktop\Langage\Python Projects\Project\Gestion de banque\images\logo.png")
-        imglogo = imglogo.resize((120,120),Image.ANTIALIAS)
+        imglogo = imglogo.resize((110,110),Image.ANTIALIAS)
         self.photoimglogo = ImageTk.PhotoImage(imglogo)
 
         lblimglogo = Label(self.root,image=self.photoimglogo,bd=4,relief=RIDGE)
-        lblimglogo.place(x=0,y=0,width=120,height=120)
+        lblimglogo.place(x=5,y=5,width=110,height=110)
 
         #title
         lbltitle = Label(self.root , text="Bank Management System",font=("times new roman",35,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
@@ -114,29 +121,38 @@ class BankManagementSys :
 
         #main frame
         mainframe = Frame(self.root , bd=4 , relief=RIDGE)
-        mainframe.place (x=200 , y= 180 , width=600 , height=800)
+        mainframe.place (x=100 , y= 180 , width=800 , height=500)
         
+        #mainbankoic
+
+        imgmain = Image.open(r"C:\Users\lenevo\Desktop\Langage\Python Projects\Project\Gestion de banque\images\mainbank.jpg")
+        imgmain = imgmain.resize((800,500),Image.ANTIALIAS)
+        self.photoimgmain = ImageTk.PhotoImage(imgmain)
+
+        lblimgmain = Label(mainframe,image=self.photoimgmain,bd=4,relief=RIDGE)
+        lblimgmain.place(x=0,y=0,width=800,height=500)
+
         #log in
         lblmain = Label(mainframe , text="Log in",font=("times new roman",20,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
-        lblmain.place(x=200,y=20,width=200)
+        lblmain.place(x=300,y=20,width=200)
 
         lbluser = Label(mainframe , text="N° Account",font=("times new roman",20,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
-        lbluser.place(x=200,y=100,width=200)
+        lbluser.place(x=300,y=100,width=200)
 
         username = Entry(mainframe,font=("arial",20,"bold"),width=60)
-        username.place(x=200 , y=150 , width=200, height=50)
+        username.place(x=300 , y=150 , width=200, height=50)
 
         lblpswd = Label(mainframe , text="Password",font=("times new roman",20,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
-        lblpswd.place(x=200,y=250,width=200)
+        lblpswd.place(x=300,y=250,width=200)
 
         password = Entry(mainframe,font=("arial",20,"bold"),width=60,show="*")
-        password.place(x=200 , y=300 , width=200, height=50)
+        password.place(x=300 , y=300 , width=200, height=50)
 
         login = Button(mainframe ,text="Login" , command=loginnow,font=("times new roman",18,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE )
-        login.place (x= 300 , y= 380 , width=150 , height=50)
+        login.place (x= 450 , y= 380 , width=150 , height=50)
         
         signup = Button(mainframe , command=self.signupdetail,text="Sign Up" ,font=("times new roman",18,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE )
-        signup.place (x= 100 , y= 380 , width=150 , height=50)
+        signup.place (x= 200 , y= 380 , width=150 , height=50)
 
     def signupdetail (self) :
         self.new_windows = Toplevel(self.root)
